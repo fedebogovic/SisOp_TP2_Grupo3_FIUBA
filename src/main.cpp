bd2ed9adcac0f10c6311fcb5eba5da30e7b2b973
Yuquimura <brianzambellitello@gmail.com>
2021-07-13 20:27:31 -0300

    New: Escribo todo lo que hay que hacer como comentario

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+//
+// Created by brian on 12/7/21.
+/* Concepto del tp.
+ * Tenemos un archivo al cual le van escribiendo O's o G's de forma continua. Para simularlo, simplemente
+ * tenemos un archivo con muchisimas O y G (MUCHISIMAS)
+ * Luego, dependiendo de si es una O o G, creamos un proceso con el nombre asociado.
+ * Vamos encolando en cada una de las listas correspondientes
+ * Luego, si vemos que hay 2 de cada lado o, 4 de un lado, desencolamos los correspondientes,
+ * escribimos la secuencia en un archivo
+ * Esto hay que ejecutarlo mientras el finalizador no nos diga que hay que terminar
+ * POR TANTO
+ * Tenemos que armar el bucle de lectura
+ * Y el creador generico de proceso
+ * Frenar cada uno de los proceso que se crean
+ * Y ver como arrancar los correspondientes
+ * */
+//
+int main(int argc, char *argv[]){
+    list<char*> otaku_list;
+    list<char*> gotics_list;
+
+}
